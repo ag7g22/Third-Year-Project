1c917353a51ea0c4b3926891987648ae8ecffe05
Anthony Geron <ag7g22@soton.ac.uk>
2025-03-05 19:45:41 +0000

    Added creating game lobbies.

--- front-end-code/server.js
+++ front-end-code/server.js
@@ -10,18 +10,108 @@ const io = require('socket.io')(server);
 
 // Serve Vue.js static files from the 'dist' folder
 app.use(express.static(path.join(__dirname, 'dist')));
-
-// Serve index.html as the entry point for Vue.js
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));  // Serve Vue's index.html
 });
 
-// Handle socket.io connections (if needed)
+// Key-Value Maps for the CHALLENGE portion.
+let users = new Map(); // (username, {username: "username", host: "host game they're in", role: [HOST/PLAYER] }) ADDED ONCE THEY'RE IN A GAME
+
+let usersToSockets = new Map(); // username -> user's socket
+let socketsToUsers = new Map(); // user's socket -> username
+
+let games = new Map(); // (host, {host: "host game", password: "password", players: ["username" ... ]})
+
+// UPDATING USERS
+function updateAllInServer(host) {
+    const theGame = games.get(host);
+    for (const username of theGame.players) {
+        const theSocket = usersToSockets.get(username);
+        update_user(theSocket, host);
+    }
+}
+
+function update_user(socket, host) {
+    const username = socketsToUsers.get(socket);
+    const theUser = users.get(username);
+    const theGame = games.get(host);
+    const data = { game_state: theGame, user_state: theUser };
+    socket.emit('update-user', data);
+    console.log('Updated user: ', username);
+}
+
+// LOBBY HANDLERS
+function handle_create_game(socket, host_username, host_password) {
+    // Check if password is used already. If so, send to user to rewrite a password
+    for (let [host, game_state] of games) {
+        if (game_state.password === host_password) {
+            socket.emit('create-game-fail');
+            return;
+        }
+    }
+
+    // Initalise states
+    let user_state = {username: host_username, host: host_username, role: "HOST"};
+    let game_state = {host: host_username, password: host_password, players: [host_username]};
+
+    // Add to the Maps:
+    users.set(host_username, user_state);
+    usersToSockets.set(host_username, socket);
+    socketsToUsers.set(socket, host_username);
+    games.set(host_username, game_state);
+
+    // Notify successful game creation
+    socket.emit('create-game-success');
+    console.log('Created NEW GAME:', host_username);
+
+    console.log([...users]);
+    console.log([...usersToSockets]);
+    console.log([...socketsToUsers]);
+    console.log([...games]);
+}
+
+function handle_delete_game(host) {
+    // Remove all players from lobby:
+    const theGame = games.get(host);
+    for (const username of theGame.players) {
+        const theSocket = usersToSockets.get(username);
+
+        // Remove from the Maps:
+        users.delete(username);
+        usersToSockets.delete(username);
+        socketsToUsers.delete(theSocket);
+
+        theSocket.emit('remove-player');
+    }
+
+    games.delete(theGame);
+    console.log('Deleted GAME: ', host);
+
+    console.log([...users]);
+    console.log([...usersToSockets]);
+    console.log([...socketsToUsers]);
+    console.log([...games]);
+}
+
+// Handle socket.io connections
 io.on('connection', socket => {
     console.log('New connected user'); 
 
     socket.on('disconnect', () => {
         console.log('User Disconnected');
+    });
+
+    socket.on('ACK', () => {
+        console.log('User sent ACK');
+        socket.emit('server_ACK');
+    });
+
+    socket.on('create-game', (host_username, host_password) => {
+        handle_create_game(socket, host_username, host_password);
+    });
+
+    socket.on('delete-game', (host) => {
+        handle_delete_game(host);
     });
 
 });
--- front-end-code/src/views/Social/Lobby.vue
+++ front-end-code/src/views/Social/Lobby.vue
@@ -1,10 +1,49 @@
 <template>
     <div class="container">
-        <h1 class="title">VERSUS LOBBY | {{ logged_in_user }}</h1>
+        <h1 class="title">CRASH QUIZ | {{ state.current_view }} | {{ logged_in_user }}</h1>
 
-        <!-- Buttons -->
+        <!-- Toogle Buttons -->
         <div class="buttons">
             <button @click="next_page('dashboard')">Back</button>
+            <button @click="send_ACK()">Test Server Comms</button>
+            <button @click="toggle_view('RULES')">Rules</button>
+            <button @click="toggle_view('HOST')">Host Game</button>
+            <button @click="toggle_view('JOIN')">Join Game</button>
+        </div>
+
+        <div v-if="state.current_view === 'RULES'">
+            <div class="instruction-box"> 
+                <p> You are against another player answering different questions </p>
+                <p> and once you click the answer immediately locks in. </p>
+                <p> There will be a car at the top of the screen. </p>
+                <p> If you get the question correct, the car will move towards your opponenent </p>
+                <p> Otherwise the car will move towards you. </p>
+                <p> Once the car crashes on a player, they lose! </p>
+            </div>
+
+        </div>
+
+        <div v-if="state.current_view === 'HOST'">
+            <h2> Create a HOST password </h2>
+            <input type="text" placeholder="password" v-model="host_password" />
+            <button @click="create_game()">Create Game</button>
+        </div>
+
+        <div v-if="state.current_view === 'JOIN'">
+            <h2> Enter a HOST password </h2>
+            <input type="text" placeholder="password" v-model="host_password" />
+            <button>Join Game</button>
+        </div>
+
+        <div v-if="state.current_view === 'LOBBY'">
+
+            <div v-if="state.role === 'HOST'">
+                <button @click="delete_game()">Delete Lobby</button>
+            </div>
+            <div v-else>
+                <button>Leave Game</button>
+            </div>
+            
         </div>
 
         <p v-if="message.error" class="error-message">{{ message.error }}</p>
@@ -18,13 +57,50 @@ import toastr from 'toastr';
 export default {
     // Page member variables and methods:
     name: "lobby",
+    mounted: function() {
+        listen(this, this.client_socket); 
+    },
     data() {
         return {
+            state: { current_view: "RULES", role: '' },
+            client_socket: this.$store.state.currentClientSocket,
+
+            // Password to join a hosted game
+            host_password: '', 
+            
+            // States updated by server:
+            game_state: null,
+            user_state: null,
+
             logged_in_user: this.$store.state.currentUser,
             message: { error: "", success: "" },
         };
     },
     methods: {
+        // Testing client-server communication
+        send_ACK() {
+            this.client_socket.emit('ACK');
+        },
+        create_game() {
+            // Send to server to register game.
+            this.client_socket.emit('create-game', this.logged_in_user, this.host_password);
+        },
+        delete_game() {
+            // Send to server to remove game.
+            this.client_socket.emit('delete-game', this.logged_in_user);
+        },
+        toggle_view(view) {
+            // Reset state
+            this.message.error = "";
+            this.message.success = "";
+            this.state.current_view = view;
+
+            if (view === "HOST") {
+                this.state.role = "HOST"
+            } else if (view === "JOIN") {
+                this.state.role = "PLAYER"
+            }
+        },
         async add_achievement(name) {
             // Add achievement to user's achievements and notify on the UI.
             this.achievements.push(name)
@@ -78,7 +154,42 @@ export default {
     },
 };
 
+function listen(vue, client_socket) {
+    console.log("Current Client: ", client_socket);
+
+    client_socket.on('server_ACK', function() {
+        console.log("Server sent ACK")
+    });
+
+    client_socket.on('create-game-success', function() {
+        vue.toggle_view('LOBBY');
+    });
+
+    client_socket.on('create-game-fail', function() {
+        vue.message.error = "Password already being used."
+    });
+
+    client_socket.on('remove-player', function() {
+        vue.toggle_view('RULES');
+        vue.message.success = "Lobby was terminated."
+    })
+
+    client_socket.on('update-user', function(data) {
+        vue.game_state = data.game_state;
+        vue.user_state = data.user_state; 
+    });
+}
+
 </script>
 
 <style lang="scss" scoped>
+.instruction-box {
+  background-color: #f9f9f9;
+  border-left: 5px solid #007bff;
+  padding: 10px;
+  margin: 10px 0;
+  font-size: 14px;
+  color: #333;
+  border-radius: 5px;
+}
 </style>
