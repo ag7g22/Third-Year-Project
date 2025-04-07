7f71570daaf7735b06fd90f3ad18a88329c50ed8
Anthony Geron <ag7g22@soton.ac.uk>
2025-04-07 15:52:09 +0100

    Social pages optimised.

--- front-end-code/src/views/Social/FriendsList.vue
+++ front-end-code/src/views/Social/FriendsList.vue
@@ -1,248 +1,207 @@
 <template>
     <div class="container">
-        <h1 class="title">SOCIAL LIST | {{ logged_in_user }}</h1>
-        
-        <!-- Toggle Buttons -->
-        <div>
-            <button @click="next_page('dashboard')">Back</button>
-            <button @click="toggle_list('friends')">Friends</button>
-            <button @click="toggle_list('requests')">Friend Requests</button>
-            <button @click="toggle_list('search')">Search Users</button>
-        </div>
-
-        <!-- Conditional List Rendering -->
-        <div v-if="current_list === 'friends'">
-            <h2>Friends List</h2>
-            <ul v-if="social_lists.friends.length">
-                <li v-for="friend in social_lists.friends" :key="friend.id">{{ friend.username }}
-                    <button @click="view_user(friend.username)"">View</button>
-                    <button @click="remove_friend(friend.id, friend.username)">Remove Friend</button>
-                </li>
-            </ul>
-        </div>
-
-        <div v-if="current_list === 'requests'">
-            <h2>Friend Requests</h2>
-            <ul v-if="social_lists.friend_requests.length">
-                <li v-for="request in social_lists.friend_requests" :key="request.id">
-                {{ request.username }} 
-                <button @click="accept_request(request.id, request.username)">Accept</button>
-                <button @click="reject_request(request.id, request.username)">Decline</button>
-                </li>
-            </ul>
-        </div>
-
-        <div v-if="current_list === 'search'">
-            <h2>Search Users</h2>
-            <input type="text" id="search" v-model="search.query" placeholder="Search..." />
-            <button @click="search_users" class="btn-search">🔍</button>
-            <ul v-if="search.users.length">
-                <li v-for="user in search.users" :key="user.id">
-                {{ user.username }}
-                <button @click="view_user(user.username)"">View</button>
-                <button v-if="!social_lists.friends.some(friend => friend.username === user.username)" @click="send_friend_request(user.id, user.username)">Add Friend</button>
-                </li>
-            </ul>
-        </div>
-
-        <p v-if="message.error" class="error-message">{{ message.error }}</p>
-        <p v-else-if="message.success" class="success-message">{{ message.success }}</p>
-    </div>
-</template>
+      <h1 class="title">SOCIAL LIST | {{ logged_in_user }}</h1>
   
-<script>
-import toastr from 'toastr';
-import 'toastr/build/toastr.min.css';
-export default {
-    // Page member variables and methods:
+      <!-- Toggle Buttons -->
+      <div>
+        <button @click="next_page('dashboard')">Back</button>
+        <button @click="toggle_list('friends')">Friends</button>
+        <button @click="toggle_list('requests')">Friend Requests</button>
+        <button @click="toggle_list('search')">Search Users</button>
+      </div>
+  
+      <!-- Friends List -->
+      <div v-if="current_list === 'friends'">
+        <h2>Friends List</h2>
+        <ul v-if="social_lists.friends.length">
+          <li v-for="friend in social_lists.friends" :key="friend.id">
+            {{ friend.username }}
+            <button @click="view_user(friend.username)">View</button>
+            <button @click="remove_friend(friend.id, friend.username)">Remove Friend</button>
+          </li>
+        </ul>
+      </div>
+  
+      <!-- Friend Requests -->
+      <div v-if="current_list === 'requests'">
+        <h2>Friend Requests</h2>
+        <ul v-if="social_lists.friend_requests.length">
+          <li v-for="request in social_lists.friend_requests" :key="request.id">
+            {{ request.username }}
+            <button @click="accept_request(request.id, request.username)">Accept</button>
+            <button @click="reject_request(request.id, request.username)">Decline</button>
+          </li>
+        </ul>
+      </div>
+  
+      <!-- Search Users -->
+      <div v-if="current_list === 'search'">
+        <h2>Search Users</h2>
+        <input type="text" v-model="search.query" placeholder="Search..." />
+        <button @click="search_users">🔍</button>
+        <ul v-if="search.users.length">
+          <li v-for="user in search.users" :key="user.id">
+            {{ user.username }}
+            <button @click="view_user(user.username)">View</button>
+            <button v-if="!is_friend(user.username)" @click="send_friend_request(user.id, user.username)">Add Friend</button>
+          </li>
+        </ul>
+      </div>
+  
+      <p v-if="message.error" class="error-message">{{ message.error }}</p>
+      <p v-else-if="message.success" class="success-message">{{ message.success }}</p>
+    </div>
+  </template>
+  
+  <script>
+  import toastr from 'toastr';
+  import 'toastr/build/toastr.min.css';
+  
+  export default {
     name: "friends",
-    mounted() {
-        this.current_list = this.$route.query.current_list || "friends";
-        this.search = this.$route.query.search || { query: "", users: [] };
-    },
     data() {
-        return {
-            // Default view
-            current_list: "friends",
-            search: { query: "", users: [] },
-            logged_in_user: this.$store.state.currentUser,
-            stats: this.$store.state.currentStats,
-            social_lists: this.$store.state.currentSocialLists,
-            message: { error: "", success: "" }
-        };
+      return {
+        current_list: this.$route.query.current_list || "friends",
+        search: this.$route.query.search || { query: "", users: [] },
+        logged_in_user: this.$store.state.currentUser,
+        stats: this.$store.state.currentStats,
+        social_lists: this.$store.state.currentSocialLists,
+        message: { error: "", success: "" }
+      };
     },
     methods: {
-        toggle_list(list) {
-            // Reset state
-            this.message.error = "";
-            this.message.success = "";
-            this.search = { query: "", users: [] };
-
-            this.current_list = list;
-        },
-        async view_user(username) {
-            // Load up user's stats to be viewed
-            const response = await this.azure_function('POST', '/user/get/info', {"username": username});
-            if (response.result) { 
-                // Collect the user's info
-                const info = response.msg;
-                const rank = info.rank;
-                const stats = { id: info.id, streak: info.streak, daily_training_score: info.daily_training_score, training_completion_date: info.training_completion_date};
-                const achievements = info.achievements;
-
-                console.log("/account");
-                this.$router.push({
-                    path: `/account`,
-                    query: { view: 'friends_list', username: username, rank: rank, stats: stats, achievements: achievements,
-                    current_list: this.current_list, search: this.search  }
-                });
-
-            } else {
-                this.message.error = response.msg || "Loading user failed.";
+      toggle_list(list) {
+        this.message = { error: "", success: "" };
+        this.search = { query: "", users: [] };
+        this.current_list = list;
+      },
+      is_friend(username) {
+        return this.social_lists.friends.some(friend => friend.username === username);
+      },
+      async view_user(username) {
+        const response = await this.azure_function('POST', '/user/get/info', { username });
+        if (response.result) {
+          const info = response.msg;
+          const routeData = {
+            path: '/account',
+            query: {
+              view: 'friends_list',
+              username,
+              rank: info.rank,
+              stats: {
+                id: info.id,
+                streak: info.streak,
+                daily_training_score: info.daily_training_score,
+                training_completion_date: info.training_completion_date
+              },
+              achievements: info.achievements,
+              current_list: this.current_list,
+              search: this.search
             }
-        },
-        async remove_friend(id, username) {
-            // Reset messages
-            this.message.error = "";
-            this.message.success = "";
-
-            // Remove both friends:
-            const input = {"id_1": this.stats.id, "id_2": id}
-            const remove = await this.azure_function("POST", "/user/friend/remove", input)
-            if (remove.result) {
-                this.message.success = 'Removed friend!'
-                
-                // Remove from the friends list
-                let friends = this.social_lists.friends;
-                let friend_requests = this.social_lists.friend_requests;
-                friends = friends.filter(friend => (friend.id !== id, friend.username !== username))
-
-                console.log('Friends:', friends)
-
-                this.$store.commit("setCurrentSocialLists", {friends: friends, friend_requests: friend_requests});
-                this.social_lists = {friends: friends, friend_requests: friend_requests}
-
-            } else {
-                this.message.error = accept.msg || "Friend removal Failed."
-            }
-        },
-        async accept_request(id, username) {
-            // Reset messages
-            this.message.error = "";
-            this.message.success = "";
-
-            // Accept the friend request:
-            const input = {"sender_id": this.stats.id, "recipient_id": id};
-            const accept = await this.azure_function("POST", "/user/friend/accept", input)
-            if (accept.result) {
-                this.message.success = 'Accepted request!';
-
-                // Remove from the friend request list and add to friend list
-                let friends = this.social_lists.friends;
-                let friend_requests = this.social_lists.friend_requests;
-                friend_requests = friend_requests.filter(friend => (friend.id !== id, friend.username !== username))
-                friends.push({'id': id, 'username': username})
-
-                console.log('Friend requests:', friend_requests)
-                console.log('Friends:', friends)
-                
-                this.$store.commit("setCurrentSocialLists", {friends: friends, friend_requests: friend_requests});
-                this.social_lists = {friends: friends, friend_requests: friend_requests}
-            } else {
-                this.message.error = accept.msg || "Friend accept Failed."
-            }
-        },
-        async reject_request(id, username) {
-            // Reset messages
-            this.message.error = "";
-            this.message.success = "";
-
-            // Reject the friend request:
-            const input = {"sender_id": id, "sender_username": username, "recipient_id": this.stats.id};
-            const reject = await this.azure_function("POST", "/user/friend/reject", input)
-            if (reject.result) {
-                this.message.success = 'Rejected request!';
-
-                // Remove from friend request list ONLY
-                let friends = this.social_lists.friends;
-                let friend_requests = this.social_lists.friend_requests;
-                friend_requests = friend_requests.filter((friend => (friend.id !== id, friend.username !== username)))
-
-                console.log('Friend requests:', friend_requests)
-                
-                this.$store.commit("setCurrentSocialLists", {friends: friends, friend_requests: friend_requests});
-                this.social_lists = {friends: friends, friend_requests: friend_requests}
-
-            } else {
-                this.message.error = reject.msg || "Friend reject Failed."
-            }
-        },
-        async send_friend_request(recipient_id, recipient_username) {
-            // Reset messages
-            this.message.error = "";
-            this.message.success = "";
-
-            // Send the friend request:
-            const input = {"sender_id": this.stats.id, "sender_username": this.logged_in_user, "recipient_id": recipient_id};
-            const search = await this.azure_function("POST", "/user/friend/request", input)
-            if (search.result) {
-                this.message.success = 'Sent request!';
-
-                // NOTIFICATION
-                const options = { "closeButton": true, "debug": false, "newestOnTop": true, "progressBar": true,
-                "positionClass": "toast-top-right", "preventDuplicates": true, "onclick": null, "showDuration": "300",
-                "hideDuration": "1000", "timeOut": "5000", "extendedTimeOut": "1000", "showEasing": "swing",
-                "hideEasing": "linear", "showMethod": "fadeIn","hideMethod": "fadeOut"}
-
-                toastr.success("Sent friend request to " + '"' + recipient_username + '"', options)
-
-            } else {
-                this.message.error = search.msg || "Friend request Failed."
-            }
-        },
-        async search_users() {
-            // Reset messages
-            this.message.error = "";
-            this.message.success = "";
-
-            // Check if the username is the same as the logged in user.
-            if (this.search.query === this.$store.state.currentUser) {
-                this.message.error = "This is you!";
-                return;
-            }
-            
-            // Search for users in the API:
-            const search = await this.azure_function("POST", "/user/search", {"username": this.logged_in_user, "search": this.search.query})
-            if (search.result) {
-                this.message.success = 'Found users!'
-                // Populate the search queries
-                this.search.users = search.msg
-            } else {
-                this.message.error = search.msg || "Username search Failed."
-            }
-        },
-        async azure_function(function_type, function_route, json_doc) {
-            console.log(function_route);
-            // Call Azure function with request
-            try {
-                const url = process.env.VUE_APP_BACKEND_URL + function_route + '?code=' + process.env.VUE_APP_MASTER_KEY
-                const response = await fetch( url, { method: function_type, headers: { "Content-Type": "application/json"},body: JSON.stringify(json_doc)});
-                const API_reply = await response.json();
-                console.log("Result: " + JSON.stringify(API_reply.result));
-                return API_reply
-            } catch (error) {
-                console.error("Error:", error);
-                this.message.error = "An API error occurred. Please try again later.";
-            }
-        },
-        next_page(page) {
-            // Move onto either dashboard or friend account page:
-            console.log("/" + page);
-            this.$router.push(`/${page}`);
+          };
+          this.$router.push(routeData);
+        } else {
+          this.message.error = response.msg || "Loading user failed.";
         }
-    },
-};
-</script>
-
-<style lang="scss" scoped>
-</style>
+      },
+      async remove_friend(id, username) {
+        this.reset_messages();
+        const input = { id_1: this.stats.id, id_2: id };
+        const response = await this.azure_function("POST", "/user/friend/remove", input);
+        if (response.result) {
+          this.message.success = 'Removed friend!';
+          const friends = this.social_lists.friends.filter(friend => friend.id !== id);
+          this.update_social_lists(friends, this.social_lists.friend_requests);
+        } else {
+          this.message.error = response.msg || "Friend removal Failed.";
+        }
+      },
+      async accept_request(id, username) {
+        this.reset_messages();
+        const input = { sender_id: this.stats.id, recipient_id: id };
+        const response = await this.azure_function("POST", "/user/friend/accept", input);
+        if (response.result) {
+          this.message.success = 'Accepted request!';
+          const friend_requests = this.social_lists.friend_requests.filter(r => r.id !== id);
+          const friends = [...this.social_lists.friends, { id, username }];
+          this.update_social_lists(friends, friend_requests);
+        } else {
+          this.message.error = response.msg || "Friend accept Failed.";
+        }
+      },
+      async reject_request(id, username) {
+        this.reset_messages();
+        const input = { sender_id: id, sender_username: username, recipient_id: this.stats.id };
+        const response = await this.azure_function("POST", "/user/friend/reject", input);
+        if (response.result) {
+          this.message.success = 'Rejected request!';
+          const friend_requests = this.social_lists.friend_requests.filter(r => r.id !== id);
+          this.update_social_lists(this.social_lists.friends, friend_requests);
+        } else {
+          this.message.error = response.msg || "Friend reject Failed.";
+        }
+      },
+      async send_friend_request(recipient_id, recipient_username) {
+        this.reset_messages();
+        const input = { sender_id: this.stats.id, sender_username: this.logged_in_user, recipient_id };
+        const response = await this.azure_function("POST", "/user/friend/request", input);
+        if (response.result) {
+          this.message.success = 'Sent request!';
+          toastr.success(`Sent friend request to "${recipient_username}"`, {
+            closeButton: true,
+            progressBar: true,
+            timeOut: 5000,
+            positionClass: "toast-top-right"
+          });
+        } else {
+          this.message.error = response.msg || "Friend request Failed.";
+        }
+      },
+      async search_users() {
+        this.reset_messages();
+        if (this.search.query === this.logged_in_user) {
+          this.message.error = "This is you!";
+          return;
+        }
+        const response = await this.azure_function("POST", "/user/search", {
+          username: this.logged_in_user,
+          search: this.search.query
+        });
+        if (response.result) {
+          this.message.success = 'Found users!';
+          this.search.users = response.msg;
+        } else {
+          this.message.error = response.msg || "Username search Failed.";
+        }
+      },
+      async azure_function(method, route, data) {
+        try {
+            console.log(route);
+          const url = `${process.env.VUE_APP_BACKEND_URL}${route}?code=${process.env.VUE_APP_MASTER_KEY}`;
+          const response = await fetch(url, {
+            method,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data)
+          });
+          return await response.json();
+        } catch (error) {
+          console.error("Azure Function Error:", error);
+          this.message.error = "An API error occurred. Please try again later.";
+        }
+      },
+      update_social_lists(friends, friend_requests) {
+        this.$store.commit("setCurrentSocialLists", { friends, friend_requests });
+        this.social_lists = { friends, friend_requests };
+      },
+      reset_messages() {
+        this.message = { error: "", success: "" };
+      },
+      next_page(page) {
+        this.$router.push(`/${page}`);
+      }
+    }
+  };
+  </script>
+  
+  <style lang="scss" scoped>
+  </style>
--- front-end-code/src/views/Social/Leaderboard.vue
+++ front-end-code/src/views/Social/Leaderboard.vue
@@ -1,103 +1,93 @@
 <template>
     <div class="container">
-        <h1 class="title">LEADERBOARD | {{ logged_in_user }}</h1>
-        <div>
-            <button @click="next_page('dashboard')">Back</button>
-            <button @click="toggle_list('public')">Public</button>
-            <button @click="toggle_list('friends')">Friends</button>
-        </div>
-
-        <!-- Conditional List Rendering -->
-
-        <div v-if="current_list === 'public'">
-            <h2>DAILY QUIZ: PUBLIC</h2>
-            <ul v-if="leaderboards.public.length">
-                <li v-for="(user, index) in leaderboards.public">
-                    #{{ index + 1 }} {{ user.username }}: {{ user.daily_training_score }} | STREAK: {{ user.streak }}
-                    <button @click="view_user(user.username)">View</button>
-                </li>
-            </ul>
-            <div v-else> 
-                <p>No users found.</p>
-            </div>
-        </div>
-
-        <div v-if="current_list === 'friends'">
-            <h2>DAILY QUIZ: FRIENDS</h2>
-            <ul v-if="leaderboards.friends.length">
-                <li v-for="(user, index) in leaderboards.friends">
-                    #{{ index + 1 }} {{ user.username }}: {{ user.daily_training_score }} | STREAK: {{ user.streak }}
-                    <button @click="view_user(user.username)">View</button>
-                </li>
-            </ul>
-            <div v-else> 
-                <p>You have no friends! Whats the point of a friend leaderboard if you have no friends you loser!</p>
-            </div>
-        </div>
-
-    </div>
-</template>
+      <h1 class="title">LEADERBOARD | {{ logged_in_user }}</h1>
   
-<script>
-export default {
-    // Page member variables and methods:
+      <div>
+        <button @click="next_page('dashboard')">Back</button>
+        <button @click="toggle_list('public')">Public</button>
+        <button @click="toggle_list('friends')">Friends</button>
+      </div>
+  
+      <!-- Leaderboard Sections -->
+      <div v-if="current_list === 'public'">
+        <h2>DAILY QUIZ: PUBLIC</h2>
+        <ul v-if="leaderboards?.public?.length">
+          <li v-for="(user, index) in leaderboards.public" :key="user.username">
+            #{{ index + 1 }} {{ user.username }}: {{ user.daily_training_score }} | STREAK: {{ user.streak }}
+            <button @click="view_user(user.username)">View</button>
+          </li>
+        </ul>
+        <p v-else>No users found.</p>
+      </div>
+  
+      <div v-else-if="current_list === 'friends'">
+        <h2>DAILY QUIZ: FRIENDS</h2>
+        <ul v-if="leaderboards?.friends?.length">
+          <li v-for="(user, index) in leaderboards.friends" :key="user.username">
+            #{{ index + 1 }} {{ user.username }}: {{ user.daily_training_score }} | STREAK: {{ user.streak }}
+            <button @click="view_user(user.username)">View</button>
+          </li>
+        </ul>
+        <p v-else>You have no friends! What's the point of a friend leaderboard if you have no friends, you loser!</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
     name: "leaderboard",
     data() {
-        return {
-            current_list: "public",
-            logged_in_user: this.$store.state.currentUser,
-            leaderboards: this.$store.state.currentLeaderboards,
-            message: { error: "", success: "" },
-        };
+      return {
+        current_list: "public",
+        logged_in_user: this.$store.state.currentUser,
+        leaderboards: this.$store.state.currentLeaderboards,
+        message: { error: "", success: "" },
+      };
     },
     methods: {
-        toggle_list(list) {
-            // Reset state
-            this.message.error = "";
-            this.message.success = "";
-            this.current_list = list;
-        },
-        async view_user(username) {
-            // Load up user's stats to be viewed
-            const response = await this.azure_function('POST', '/user/get/info', {"username": username});
-            if (response.result) { 
-                // Collect the user's info
-                const info = response.msg;
-                const rank = info.rank;
-                const stats = { id: info.id, streak: info.streak, daily_training_score: info.daily_training_score, training_completion_date: info.training_completion_date};
-                const achievements = info.achievements;
-
-                console.log("/account");
-                this.$router.push({
-                    path: `/account`,
-                    query: { view: 'leaderboard', username: username, rank: rank, stats: stats, achievements: achievements}
-                });
-
-            } else {
-                this.message.error = response.msg || "Loading user failed.";
-            }
-        },
-        async azure_function(function_type, function_route, json_doc) {
-            console.log(function_route);
-            // Call Azure function with request
-            try {
-                const url = process.env.VUE_APP_BACKEND_URL + function_route + '?code=' + process.env.VUE_APP_MASTER_KEY
-                const response = await fetch( url, { method: function_type, headers: { "Content-Type": "application/json"},body: JSON.stringify(json_doc)});
-                const API_reply = await response.json();
-                console.log("Result: " + JSON.stringify(API_reply.result));
-                return API_reply
-            } catch (error) {
-                console.error("Error:", error);
-                this.message.error = "An API error occurred. Please try again later.";
-            }
-        },
-        next_page(page) {
-            console.log("/" + page);
-            this.$router.push(`/${page}`);
+      toggle_list(list) {
+        this.message = { error: "", success: "" };
+        this.current_list = list;
+      },
+      async view_user(username) {
+        const response = await this.azure_function("POST", "/user/get/info", { username });
+        if (response?.result) {
+          const { id, streak, daily_training_score, training_completion_date, achievements, rank } = response.msg;
+  
+          this.$router.push({
+            path: "/account",
+            query: {
+              view: "leaderboard",
+              username,
+              rank,
+              stats: { id, streak, daily_training_score, training_completion_date },
+              achievements,
+            },
+          });
+        } else {
+          this.message.error = response?.msg || "Loading user failed.";
         }
+      },
+      async azure_function(method, route, data) {
+        try {
+          const url = `${process.env.VUE_APP_BACKEND_URL}${route}?code=${process.env.VUE_APP_MASTER_KEY}`;
+          const response = await fetch(url, {
+            method,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data),
+          });
+          return await response.json();
+        } catch (error) {
+          console.error("Azure Function Error:", error);
+          this.message.error = "An API error occurred. Please try again later.";
+        }
+      },
+      next_page(page) {
+        this.$router.push(`/${page}`);
+      },
     },
-};
-</script>
-
-<style lang="scss" scoped>
-</style>
+  };
+  </script>
+  
+  <style lang="scss" scoped>
+  </style>
